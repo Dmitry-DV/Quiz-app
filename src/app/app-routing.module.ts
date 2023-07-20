99f2df9d4946376ce58f4352a953872e268acd92
дмитрий журбенко <dmitrijzurbenko@MacBook-Air-dmitrij.local>
2023-07-20 11:58:02 +0400

    The main component has been created. Setting up routing for main and loyalt(header, footer)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoltComponent } from './shared/layolt/layolt.component';
+import { MainComponent } from './views/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoltComponent,
+    children: [
+      {path: '', component: MainComponent}
+      // loadChildren: (): => import('./').then(m => m..)
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { LayoltComponent } from './shared/layolt/layolt.component';
 import { HeaderComponent } from './shared/layolt/header/header.component';
 import { FooterComponent } from './shared/layolt/footer/footer.component';
+import { MainComponent } from './views/main/main.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoltComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
