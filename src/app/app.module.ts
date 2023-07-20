ea3bae025419121a2fa5a39cd5ed9fb656cc8e68
дмитрий журбенко <dmitrijzurbenko@MacBook-Air-dmitrij.local>
2023-07-20 11:15:47 +0400

    header component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoltComponent } from './shared/layolt/layolt.component';
+import { HeaderComponent } from './shared/layolt/header/header.component';
+import { FooterComponent } from './shared/layolt/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoltComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/layolt/layolt.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layolt',
+  templateUrl: './layolt.component.html'
+})
+export class LayoltComponent {
+
+}
